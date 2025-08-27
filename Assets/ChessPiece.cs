61eba1de0a8a1ca0d69787b7048e85201ca6e123
da413 <danielwolfcol@gmail.com>
2025-08-27 14:48:18 -0400

    split sprite into sub sprites

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class ChessPiece : MonoBehaviour
+
+// Shows info of a GameObject depending on the selected option
+
+public enum OPTIONS
 {
-    // Start is called before the first frame update
-    void Start()
+    Position = 0,
+    Rotation = 1,
+    Scale = 2,
+}
+
+
+public class EditorGUIEnumPopup : EditorWindow
+{
+    OPTIONS display = OPTIONS.Position;
+
+    [MenuItem("Examples/Editor GUI Enum Popup usage")]
+    static void Init()
     {
-        
+        EditorWindow window = GetWindow(typeof(EditorGUIEnumPopup));
+        window.position = new Rect(0, 0, 220, 80);
+        window.Show();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        Transform selectedObj = Selection.activeTransform;
+
+        display = (OPTIONS)EditorGUI.EnumPopup(
+            new Rect(3, 3, position.width - 6, 15),
+            "Show:",
+            display);
+
+        EditorGUI.LabelField(new Rect(0, 20, position.width, 15),
+            "Name:",
+            selectedObj ? selectedObj.name : "Select an Object");
+        if (selectedObj)
+        {
+            switch (display)
+            {
+                case OPTIONS.Position:
+                    EditorGUI.LabelField(new Rect(0, 40, position.width, 15),
+                        "Position:",
+                        selectedObj.position.ToString());
+                    break;
+
+                case OPTIONS.Rotation:
+                    EditorGUI.LabelField(new Rect(0, 40, position.width, 15),
+                        "Rotation:",
+                        selectedObj.rotation.ToString());
+                    break;
+
+                case OPTIONS.Scale:
+                    EditorGUI.LabelField(new Rect(0, 40, position.width, 15),
+                        "Scale:",
+                        selectedObj.localScale.ToString());
+                    break;
+
+                default:
+                    Debug.LogError("Unrecognized Option");
+                    break;
+            }
+        }
+
+        if (GUI.Button(new Rect(3, position.height - 25, position.width - 6, 24), "Close"))
+            this.Close();
     }
 }
