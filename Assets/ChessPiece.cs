b34a25ad4a9e462ce765d100fe540108136f70cc
Victor Avila <victoraa1116@gmail.com>
2025-08-27 14:48:45 -0400

    Update ChessPiece.cs

--- Assets/ChessPiece.cs
+++ Assets/ChessPiece.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class ChessPiece : MonoBehaviour
 {
+    [SerializeField] public enum PieceType
+    {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,10 @@ public class ChessPiece : MonoBehaviour
     void Update()
     {
         
+    }
+    public Texture chessTexture;
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawGUITexture(new Rect(0,0,1,1), chessTexture);
     }
 }
